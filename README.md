edd9196cf30524acc90740abb033a2e46b9db0f0
firefox94 <haviet20@gmail.com>
2025-07-09 22:46:41 +0700

    first push

---
+++ README.md
@@ -0,0 +1,23 @@
+🧩 Tired of limited AI tools that depend on the cloud, cap your queries, and can’t handle local files? Then check this out 👇
+
+🚀 Excited to Share something really cool here: A Local Chatbot Agent with Streamlit!
+
+✅ Works Offline – No internet? No problem. The model runs locally without relying on cloud APIs.
+
+✅ Unlimited Queries – Ask as much as you want — no restrictions.
+
+✅ Private & Secure – All processing stays on your machine. Your documents never leave your environment.
+
+✅ Customizable – Modify prompts, logic, or plug in your own data. You have full control.
+
+✅ Local Document Analysis – Upload PDFs, text files, or csv files — and get answers, summaries, or insights instantly.
+
+✅ Nicely interface with Streamlit.
+
+Whether you're working in a secure environment, dealing with sensitive data, or just want to avoid vendor lock-in — this solution is flexible, lightweight, and effective.
+
+Built with:
+ 🧠 Ollama + LangChain
+ 📦 Local LLMs
+ 🎛️ Streamlit for UI
+ 📂 ChromaDB for vector storage
---
+++ app.py
@@ -0,0 +1,100 @@
+import fitz  # PyMuPDF
+from langchain_ollama import OllamaEmbeddings
+from langchain_core.documents import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_chroma import Chroma
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# 1. load your document
+pdf_path = "./movie_reviews.pdf" # your document path
+doc = fitz.open(pdf_path)
+
+documents = []
+
+for page_num in range(len(doc)):
+    page = doc[page_num]
+    text = page.get_text()
+    if text.strip():  # skip white space
+        document = Document(
+            page_content=text,
+            metadata={"page": page_num + 1}  # mark your metadata in case you wanna know which pages have been used
+        )
+        documents.append(document)
+
+doc.close()
+
+#2. create embedding instance
+embeddings = OllamaEmbeddings(model="mxbai-embed-large")
+
+#3. create chroma db to store your document
+db_location = "./chroma_db" # location where you wanna store your db
+vector_store = Chroma(
+    collection_name="your-document",
+    persist_directory=db_location,
+    embedding_function=embeddings
+)
+
+#4. store document in vector store and keep renewing your db whenever you run this app
+vector_store.add_documents(documents)
+
+#5. create retriever
+retriever = vector_store.as_retriever(search_kwargs={"k": 5}) # set k base on how many docs to return
+
+#6. setup streamlit interface
+st.set_page_config(page_title="Streaming Agent", page_icon="🤖")
+st.title("VietHa-knowyourdocs ♌")
+
+#7. define a function to get the response from your agent
+def get_response(query, chat_history):
+    template = """
+    You are an expert in so many domains, especialty good at summarizing information.
+    Chat history: {chat_history}
+    Here are the data need to summary: {context}
+    Here is the question: {query}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+    model = OllamaLLM(model="llama3.2")
+    chain = prompt | model
+    context = retriever.invoke(query)
+    
+    return chain.invoke({
+        "chat_history": chat_history,
+        "context": context,
+        "query": query
+    })
+    
+#8. build conversation flow
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+for message in st.session_state.chat_history:
+    if isinstance(message, HumanMessage):
+        with st.chat_message("human"):
+            st.markdown(message.content)
+    elif isinstance(message, AIMessage):
+        with st.chat_message("ai"):
+            st.markdown(message.content)
+    else:
+        st.warning(f"Unsupported message type {type(message)}")
+        
+#9. user input query
+query = st.chat_input("What do you want to know about your documents?")
+if query is not None and query != "":
+    st.session_state.chat_history.append(HumanMessage(query))
+    
+    with st.chat_message("human"):
+        st.markdown(query)
+    
+    with st.chat_message("ai"):
+        ai_response = get_response(query,st.session_state.chat_history)
+        st.markdown(ai_response)
+        
+    st.session_state.chat_history.append(AIMessage(content=ai_response))
+       
